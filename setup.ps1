ee5cfae0f23180ace7da478811d65e2b3a0132cf
unknown <paulo.techie@gmail.com>
2023-07-13 00:18:41 +0300

    changes

--- setup.ps1
+++ setup.ps1
@@ -11,22 +11,6 @@ $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
 # Get the latest commit hash of the remote "master" branch
 $remoteCommitHash = git -C $scriptDirectory ls-remote origin "refs/heads/master" | ForEach-Object { $_.Split()[0] }
 
-# Check if there are changes in the remote "master" branch for the specified folders
-$hasRemoteChanges = $false
-foreach ($folder in $foldersToMonitor) {
-    $localCommitHash = git -C (Join-Path $scriptDirectory $folder) rev-parse master
-    if ($localCommitHash -ne $remoteCommitHash) {
-        Write-Host "There are changes in remote 'master' branch for folder '$folder'."
-        $hasRemoteChanges = $true
-    }
-}
-
-# If there are remote changes, urge a pull first
-if ($hasRemoteChanges) {
-    Write-Host "Please pull the latest changes before continuing."
-    return
-}
-
 # Check for conflicting changes
 $hasConflictingChanges = $false
 foreach ($folder in $foldersToMonitor) {
